15739e523cb45c9831491d52c8d9de89dc7c8a0a
codenextitD <engr.ismail348@gmail.com>
2022-02-28 02:26:45 +0600

    install admin dashboard and linking

---
+++ resources/views/admin/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.admin')
+
+@section('content')
+
+    <div class="card">
+        <div class="card-body">
+            <h1>Code Next IT</h1>
+        </div>
+    </div>
+
+@endsection
